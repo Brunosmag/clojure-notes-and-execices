259fba9039efb305e12df2c96f3f8a28f543c8b0
Brunosmag <43658894+Brunosmag@users.noreply.github.com>
2021-09-07 19:38:29 -0300

    Update exercicios.clj

--- Exercicios/exercicios.clj
+++ Exercicios/exercicios.clj
@@ -1,3 +1,16 @@
+;;https://www.urionlinejudge.com.br/judge/pt/problems/view/1000
+(println "Hello World!")
+
+
+
+;;https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+(def input1 (read-line))
+(def input2 (read-line))
+
+(println "X =" (+ (Long/parseLong input1)  (Long/parseLong input2)))
+
+
+
 ;;https://www.urionlinejudge.com.br/judge/pt/problems/view/1002
 (require '[clojure.math.numeric-tower :as math])
 
