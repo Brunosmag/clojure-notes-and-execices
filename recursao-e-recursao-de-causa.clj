156305d81a0777e564a0b193dd18a47e86d34098
Brunosmag <43658894+Brunosmag@users.noreply.github.com>
2021-09-08 15:18:24 -0300

    Create recursao-e-recursao-de-causa.clj

---
+++ recursao-e-recursao-de-causa.clj
@@ -0,0 +1,3 @@
+;;podemos utilizar o conceito de recursão para chamar uma função dentro dela mesmo (como se fosse um laço).
+;;usar recursão com cuidado (devido a utilização de memoria)
+;;utilizar recur para indicar a recursão e permitir que o compilador faça as otimizações de memoria.
