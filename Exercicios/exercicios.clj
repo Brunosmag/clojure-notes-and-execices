bee20b2440c670619f6bffa126c5922c9173c394
Brunosmag <43658894+Brunosmag@users.noreply.github.com>
2021-09-09 18:31:05 -0300

    Update exercicios.clj

--- Exercicios/exercicios.clj
+++ Exercicios/exercicios.clj
@@ -51,3 +51,19 @@
     (/ (+ peso-A peso-B) peso-total)))
 
 (println "MEDIA =" (format "%.5f" (calcular-media A B)))
+
+
+
+;;https://www.urionlinejudge.com.br/judge/pt/problems/view/1006
+(def A (Double/parseDouble (read-line)))
+(def B (Double/parseDouble (read-line)))
+(def C (Double/parseDouble (read-line)))
+
+(defn calcula-media
+  [A B C]
+  (let [peso-A (* A 2)
+        peso-B (* B 3)
+        peso-C (* C 5)]
+    (/ (+ peso-A peso-B peso-C) 10)))
+
+(println "MEDIA = " (calcula-media A B C))
